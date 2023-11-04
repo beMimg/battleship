921aac0d6b373c4ea6ebdfb50d337e699593f675
bernyig <bernardommguerreiro@gmail.com>
2023-11-04 18:16:18 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+const sum = (a, b) => a + b;
+
+module.exports = sum;
---
+++ tests/print.test.js
@@ -0,0 +1,5 @@
+const sum = require('../src/print.js');
+
+it('s', () => {
+  expect(sum(1, 2)).toBe(3);
+});
