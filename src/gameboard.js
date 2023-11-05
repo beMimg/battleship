07b8b659d41d7d01bece6032462270a78db87522
bernyig <bernardommguerreiro@gmail.com>
2023-11-05 22:45:18 +0100

    gameboard factory function with buildboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,34 +1,15 @@
-/* eslint-disable */
-
-// const gameboard = () => {
-//   const buildBoard = () => {
-//     const board = [];
-
-//     for (let i = 0; i < 10; i++) {
-//       for (let j = 0; j < 10; j++) {
-//         board.push([i], [j]);
-//       }
-//     }
-//     return board;
-//   };
-
-//   return { buildBoard };
-// };
-
-class Gameboard {
-  constructor() {}
-
-  buildBoard() {
+const Gameboard = () => {
+  const buildBoard = () => {
     const board = [];
-
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < 10; i += 1) {
+      for (let j = 0; j < 10; j += 1) {
         board.push([i], [j]);
       }
     }
-    console.log(board);
-  }
-}
-const gameBoard = new Gameboard();
-gameBoard.buildBoard();
-console.log(gameBoard);
+    return board;
+  };
+
+  return { buildBoard };
+};
+
+const gameboard = Gameboard();
