c92e209986b46f22bb5e7a787ee49f50fe51cfad
bernyig <bernardommguerreiro@gmail.com>
2023-11-05 17:29:11 +0100

    factory to class

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,24 @@
 /* eslint-disable */
 
-const gameboard = () => {
-  const buildBoard = () => {
+// const gameboard = () => {
+//   const buildBoard = () => {
+//     const board = [];
+
+//     for (let i = 0; i < 10; i++) {
+//       for (let j = 0; j < 10; j++) {
+//         board.push([i], [j]);
+//       }
+//     }
+//     return board;
+//   };
+
+//   return { buildBoard };
+// };
+
+class Gameboard {
+  constructor() {}
+
+  buildBoard() {
     const board = [];
 
     for (let i = 0; i < 10; i++) {
@@ -9,12 +26,9 @@ const gameboard = () => {
         board.push([i], [j]);
       }
     }
-    return board;
-  };
-
-  return { buildBoard };
-};
-
-const gameBoard = gameboard();
-const board = gameBoard.buildBoard();
-console.log(board);
+    console.log(board);
+  }
+}
+const gameBoard = new Gameboard();
+gameBoard.buildBoard();
+console.log(gameBoard);
