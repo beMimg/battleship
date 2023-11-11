59379b1ee75e3682e753d4b5cbbd2fb7b16b5016
bernyig <bernardommguerreiro@gmail.com>
2023-11-11 01:07:49 +0100

    small change on error()

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-console */
 const error = (type) => {
   let errorMessage = '';
-  if (type === 'occupied') {
-    errorMessage += 'This coordenates are already occupied';
-  } else if (type === 'placement') {
+  if (type === 'placement') {
     errorMessage += 'This placement is not allowed.';
   }
   console.error(errorMessage);
-  return errorMessage;
 };
 
 module.exports = error;
